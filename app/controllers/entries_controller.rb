7d9453f3f5c18da6e00a5dc2944bf1ee4f8be120
Lazigerbill <chaukbill@gmail.com>
2015-08-20 12:44:43 -0400

    stashing for now

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,6 @@
 class EntriesController < ApplicationController
   def landing
-    
+
   end
   def index
     @entries = Entry.all
@@ -19,7 +19,9 @@ class EntriesController < ApplicationController
 
   def create
     @input = params[:entry]["transcript"]
-    analyse(@input)
+    
+    #post request to Watson API
+    #analyse(@input)
 
     @entry = Entry.new(entry_params)  
     @entry.insights = @watson_says
@@ -41,67 +43,92 @@ class EntriesController < ApplicationController
   end
 
   def upload
-    @entry = Entry.new
     uploaded_file = params[:file]
-    # split of text files into array of sections
-    data = uploaded_file.read.split(/[\r\n]+|\={2}|\-{2}/).reject{|s| s.empty?}
- 
-    # extract event details
-    @entry.company_name = data[2]
-    @entry.event_name = data[3]
-    @entry.date = DateTime.parse(data[4])
-  
-    # extract speaker and presentations
-    parse_presentation(data)
 
-    render :new
+    #check if upload file is txt or zip
+    if File.extname(uploaded_file.original_filename) == ".txt"
+      # split of text files into array of sections
+      data = uploaded_file.read.split(/[\r\n]+|\={2}|\-{2}/).reject{|s| s.empty?}
+      event = data.first(5)
+      # extract speaker and presentations
+      parse_presentation(data)
+      bypass_input(event)
+    elsif File.extname(uploaded_file.original_filename) == ".zip"
+      #write code 
+    else
+      redirect_to new_entry_path, :alert => "File type is not supoorted!  Please try again."
+    end
   end
 end
 
 private
-  def analyse(input)
-    profile_api_url = "#{Figaro.env.bluemix_url}/v2/profile"
+def analyse(input)
+  profile_api_url = "#{Figaro.env.bluemix_url}/v2/profile"
 
-    client = RestClient::Resource.new(profile_api_url, Figaro.env.bluemix_username, Figaro.env.bluemix_password)
-    insights = client.post input, :content_type => "text/plain;charset=utf-8"
+  client = RestClient::Resource.new(profile_api_url, Figaro.env.bluemix_username, Figaro.env.bluemix_password)
+  insights = client.post input, :content_type => "text/plain;charset=utf-8"
 
-    @watson_says = JSON.load(insights.body)
+  @watson_says = JSON.load(insights.body)
+end
+
+def entry_params
+  params.require(:entry).permit(:company_name, :event_name, :date, :speaker_name, :speaker_title, :transcript)
+end
+
+def parse_presentation(data)
+  @result = []
+  # presentation will only contain the presentation section
+  data.shift(data.index("Presentation")+1)
+  presentation = data.take(data.index("Questions and Answers"))
+  
+  #create speaker index, starts at [0](usually operator)
+  speakers = presentation.select{|i| i.match(/\[[0-9]+\]/)}
+  speaker_index = []
+  speakers.each do |speaker|
+    speaker_index << presentation.index(speaker)
   end
 
-  def entry_params
-      params.require(:entry).permit(:company_name, :event_name, :date, :speaker_name, :speaker_title, :transcript)
+  #producing arrays of conversations, each element = [speaker, content], combining contents to unique speakers
+  i = 0
+  speaker_index.each do |sentence|
+    if i < speaker_index.count-1
+      if @result.assoc(presentation[sentence][/([^\[]+)/].strip!) 
+        @result[@result.index(@result.assoc(presentation[sentence][/([^\[]+)/].strip!))][1] << presentation[sentence+1..speaker_index[i+1]-1].join.strip!
+      else
+        @result << [presentation[sentence][/([^\[]+)/].strip!, presentation[sentence+1..speaker_index[i+1]-1].join.strip!]
+      end
+    else 
+      if @result.assoc(presentation[sentence][/([^\[]+)/].strip!)
+        @result[@result.index(@result.assoc(presentation[sentence][/([^\[]+)/].strip!))][1] << presentation[sentence+1..presentation.count-1].join.strip!
+      else
+        @result << [presentation[sentence][/([^\[]+)/].strip!, presentation[sentence+1..presentation.count-1].join.strip!]
+      end
     end
+    i+=1
+  end
+end
 
-  def parse_presentation(data)
-    @result = []
-
-    # presentation will only contain the presentation section
-    data.shift(data.index("Presentation")+1)
-    presentation = data.take(data.index("Questions and Answers"))
-    
-    #create speaker index, starts at [0](usually operator)
-    speakers = presentation.select{|i| i.match(/\[[0-9]+\]/)}
-    speaker_index = []
-    speakers.each do |speaker|
-      speaker_index << presentation.index(speaker)
-    end
-
-    #producing arrays of conversations, each element = [speaker, content], combining contents to unique speakers
-    i = 0
-    speaker_index.each do |sentence|
-      if i < speaker_index.count-1
-        if @result.assoc(presentation[sentence][/([^\[]+)/].strip!) 
-          @result[@result.index(@result.assoc(presentation[sentence][/([^\[]+)/].strip!))][1] << presentation[sentence+1..speaker_index[i+1]-1].join.strip!
-        else
-          @result << [presentation[sentence][/([^\[]+)/].strip!, presentation[sentence+1..speaker_index[i+1]-1].join.strip!]
-        end
-      else 
-        if @result.assoc(presentation[sentence][/([^\[]+)/].strip!)
-          @result[@result.index(@result.assoc(presentation[sentence][/([^\[]+)/].strip!))][1] << presentation[sentence+1..presentation.count-1].join.strip!
-        else
-          @result << [presentation[sentence][/([^\[]+)/].strip!, presentation[sentence+1..presentation.count-1].join.strip!]
+  def bypass_input(data)
+    @rcount = 0
+    @result.each do |pres|
+      if pres[1].split.count > 500
+        @entry = Entry.new
+        @entry.company_name = data[2].match(/-(.+?)$/)[1].strip
+        @entry.ticker = data[2].match(/^(.+?)-/)[1].strip
+        data[3].gsub!(/[^0-9a-z ]/i, '')
+        data[3].slice! (@entry.company_name+ ' ')
+        @entry.event_name = data[3]
+        @entry.date = DateTime.parse(data[4])
+        @entry.speaker_name = pres[0].match(/^(.+?),/)[1]
+        @entry.speaker_title = pres[0].match(/,(.+?)$/)[1].strip
+        @entry.transcript = pres[1]
+        @entry.wcount = pres[1].split.count
+        if @entry.save!
+          @rcount += 1
+        else render :new, :alert => "Something went wrong..."
         end
       end
-      i+=1
     end
+    redirect_to entries_path, :notice => "#{@rcount} #{'script'.pluralize(@rcount)} successfully saved!!"
   end
+
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,13 +1,15 @@
 class Entry
   include Mongoid::Document
   field :company_name
+  field :ticker
   field :event_name
   field :date, type: DateTime
   field :speaker_name
   field :speaker_title
+  field :wcount, type: Integer
   field :transcript
   field :insights, type: Hash
 
   # validations
-  validates_presence_of :company_name, :event_name, :date, :speaker, :transcript, :wcount
+  validates_presence_of :company_name, :event_name, :date, :speaker_name, :speaker_title, :transcript
 end
