f2588879b57720965ebc789e3d1de9f548acb061
Lazigerbill <chaukbill@gmail.com>
2015-08-31 13:11:46 -0400

    next step is to working on tables

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -12,6 +12,5 @@ class Entry
 
   # validations
   validates_presence_of :company_name, :ticker, :event_name, :date, :speaker_name, :speaker_title, :wcount, :transcript
-  validates_uniqueness_of :company_name, :scope => :speaker_name
-
+  validates_uniqueness_of :event_name, :scope => :speaker_name, :scope => :company_name
 end
