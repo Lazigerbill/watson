0df11df8d086908a28d33a154648e68572b4c299
Lazigerbill <chaukbill@gmail.com>
2016-02-17 22:53:51 -0500

    generated controller for forget password

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
 
+  get 'password_resets/create'
+
+  get 'password_resets/edit'
+
+  get 'password_resets/update'
+
   get 'user_sessions/new'
 
   get 'user_sessions/create'
