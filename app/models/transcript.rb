ea33b5ab58307279b520e316be13c3f23867909e
Lazigerbill <chaukbill@gmail.com>
2016-02-20 01:23:20 -0500

    removed model transcripts

--- app/models/transcript.rb
+++
@@ -1,4 +0,0 @@
-class Transcript < ActiveRecord::Base
-  validates_presence_of :company_name, :ticker, :event_name, :date, :speaker_name, :speaker_title, :wcount, :transcript
-  validates_uniqueness_of :event_name, :scope => :speaker_name, :scope => :company_name
-end
--- test/models/transcript_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TranscriptTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
