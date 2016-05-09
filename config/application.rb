db24b49e4e1cc4cc2017c0e141ba81b804e7527f
johnson <johnsonjia47@gmail.com>
2016-05-09 17:24:10 -0400

    User model RSpec Testcase.
    Coverage includes:
    Valid User sanity check
    Invalid with missing field
    Invalid with duplicate email or student id
    Invalid with non integer student id
    Invalid with student id length < 9
    Invalid with non utoronto.ca email
    Invalid with password length < 3
    Invalid with unconfirmed password

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,16 @@ module RailsStarter
     # config.mongoid.preload_models = false
     # Mongoid.logger.level = Logger::DEBUG
     # Moped.logger.level = Logger::DEBUG
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+    end
   end
 end
+
