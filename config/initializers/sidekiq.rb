f9acb82b4549759704dcf7906d31f44dd1510c17
Lazigerbill <chaukbill@gmail.com>
2016-07-11 16:27:15 -0400

    edited sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,10 @@
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: 'redis://redis.localhost:6379' }
-# end
+# This configures Sidekiq to use ENV variable, PROD connects to Redis-to-go
+# uri = URI.parse(Figaro.env.redis_uri)
+
+Sidekiq.configure_server do |config|
+  config.redis = { :url => Figaro.env.REDISTOGO_URL }
+end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Figaro.env.REDISTOGO_URL }
+  config.redis = { :url => Figaro.env.REDISTOGO_URL }
 end
